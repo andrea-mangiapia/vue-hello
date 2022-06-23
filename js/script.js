b87817d1a738c55d1a5813202dd53588de7d5fd4
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-06-23 16:17:32 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        title: 'Questo è il titolo della pagina',
+        image: 'https://thesecondangle.com/wp-content/uploads/2022/03/high-gaming-pc.jpg'
+    }
+});
